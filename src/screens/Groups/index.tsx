e26b9ea71dddaba5b74c4c5c6d84d954d329d60f
Renan Fernandes <renanfernandes-@outlook.com>
2022-11-09 12:14:04 -0300

    add ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,12 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import * as S from './styles';
 
 export function Groups() {
-    const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+    const [groups, setGroups] = useState<string[]>([]);
     return (
         <S.Container>
             <Header />
@@ -17,6 +18,10 @@ export function Groups() {
                 data={groups}
                 keyExtractor={(item) => item}
                 renderItem={({ item }) => <GroupCard title={item} />}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty message="Que tal cadastrar a primeira turma?" />
+                )}
             />
         </S.Container>
     );
