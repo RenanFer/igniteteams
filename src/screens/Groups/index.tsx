5d1c731c1f5cb72b6ae438bfdb814dffbccd0518
Renan Fernandes <renanfernandes-@outlook.com>
2022-11-09 11:56:49 -0300

    add GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 import * as S from './styles';
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
     return (
         <S.Container>
             <Header />
             <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+            <FlatList
+                data={groups}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => <GroupCard title={item} />}
+            />
         </S.Container>
     );
 }
