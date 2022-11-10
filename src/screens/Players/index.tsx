192c4e9b27bc7ef2a985ac54b3784839b2857c10
Renan Fernandes <renanfernandes-@outlook.com>
2022-11-10 11:34:40 -0300

    add players/newgroup

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,16 @@
+import * as S from './styles';
+
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+
+export function Players() {
+    return (
+        <S.Container>
+            <Header showBackButton />
+            <Highlight
+                title="Nome da turma"
+                subtitle="adicione a galera e separe os times"
+            />
+        </S.Container>
+    );
+}
