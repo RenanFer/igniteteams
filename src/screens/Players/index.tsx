36c34f8867c9ffc33a62a053a36df3d1d4fe5b21
Renan Fernandes <renanfernandes-@outlook.com>
2022-11-11 14:21:15 -0300

    finished styles

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -8,10 +8,13 @@ import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Players() {
     const [team, setTeam] = useState('Time A');
-    const [players, setPlayers] = useState(['rodrigo', 'tiago']);
+    const [players, setPlayers] = useState([]);
     return (
         <S.Container>
             <Header showBackButton />
@@ -37,8 +40,25 @@ export function Players() {
                     )}
                     horizontal
                 />
-                <S.NumbersOfPlayers>{players.length}</S.NumbersOfPlayers>
+                <S.NumberOfPlayers>{players.length}</S.NumberOfPlayers>
             </S.HeaderList>
+            <FlatList
+                data={players}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <PlayerCard name={item} onRemove={() => {}} />
+                )}
+                ListEmptyComponent={() => (
+                    <ListEmpty message="Não há pessoas nesse time" />
+                )}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[
+                    { paddingBottom: 100 },
+                    players.length === 0 && { flex: 1 },
+                ]}
+            />
+
+            <Button title="Remover Turma" type="SECONDARY" />
         </S.Container>
     );
 }
