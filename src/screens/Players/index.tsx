c5cb9b00f175f4aaecc3953b8fec5f146d47049b
Renan Fernandes <renanfernandes-@outlook.com>
2022-11-11 12:41:35 -0300

    add buttonicon/filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,9 +1,17 @@
+import { FlatList } from 'react-native';
+import { useState } from 'react';
+
 import * as S from './styles';
 
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
 
 export function Players() {
+    const [team, setTeam] = useState('Time A');
+    const [players, setPlayers] = useState(['rodrigo', 'tiago']);
     return (
         <S.Container>
             <Header showBackButton />
@@ -11,6 +19,26 @@ export function Players() {
                 title="Nome da turma"
                 subtitle="adicione a galera e separe os times"
             />
+            <S.Form>
+                <Input placeholder="Nome da pessoa" autoCorrect={false} />
+                <ButtonIcon icon="add" />
+            </S.Form>
+
+            <S.HeaderList>
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor={(item) => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+                <S.NumbersOfPlayers>{players.length}</S.NumbersOfPlayers>
+            </S.HeaderList>
         </S.Container>
     );
 }
